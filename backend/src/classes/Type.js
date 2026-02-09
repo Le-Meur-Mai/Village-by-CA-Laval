6c388c1de3fc6477b3bafa2c9474ca7983ea793b
Le-Meur-Mai <mai.lm@outlook.fr>
2026-02-09 16:11:50 +0100

    Add: Three new classes: Picture that we will relate to Cloudinary later, StartUp and Type

---
+++ backend/src/classes/Type.js
@@ -0,0 +1,49 @@
+import BaseModel from "./BaseModel.js";
+
+export default class Type extends BaseModel {
+  constructor({name, color, startUps = []}) {
+    super();
+    this.name = name;
+    this.color = color;
+    this.startUps = startUps;
+  }
+
+  set name(value) {
+    if (typeof value !== "string") {
+      throw new TypeError("Name must be a string.");
+    }
+    else if (value.length > 30) {
+      throw new Error("Name is too long.");
+    }
+    this._name = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set color(value) {
+    if (typeof value !== "string") {
+      throw new TypeError("Color must be a string.");
+    }
+    else if (value.length !== 6) {
+      throw new Error("Color must be composed of 6 characters.");
+    }
+    this._color = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set startUps(value) {
+    if (Array.isArray(value) !== true) {
+      throw new TypeError("StartUps must be an array.");
+    }
+    this._startUps = value;
+  }
+
+  get startUps() {
+    return this._startUps;
+  }
+}
