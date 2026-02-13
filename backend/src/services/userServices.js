087cd8379be93210549c7d33b061c5d881038cf2
Le-Meur-Mai <mai.lm@outlook.fr>
2026-02-13 18:13:10 +0100

    Add: The UserService class wich use the class UserRepository methods and class User, to validate all the data send, and validate the buisness logic

---
+++ backend/src/services/userServices.js
@@ -0,0 +1,59 @@
+import UserRepository from "../repositories/UserRepository.js";
+import User from "../classes/User.js";
+import prisma from "../prismaClient.js";
+import * as Errors from "../errors/errorsHandler.js"
+
+export default class UserServices {
+  constructor() {
+    // Création d'une instance pour utiliser les méthodes de la classe repo
+    this.repo = new UserRepository(prisma);
+  }
+
+  async createUser(data) {
+    try {
+      new User(data);
+      // Importer fonction de hachage et remplacer le mdp de data
+      return await this.repo.createUser(data);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getUserById(id) {
+    try {
+      const user = await this.repo.getUserById(id);
+      if (!user) {
+        throw new Errors.NotFoundError('User not found');
+      }
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getAllUsers() {
+    try {
+      return await this.repo.getAllUsers();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updateUser(id, data) {
+    try {
+      // Vérifie que le User existe
+      const existingUser = await this.repo.getUserById(id);
+      if (!existingUser) {
+        throw new Errors.NotFoundError('User not found');
+      }
+      /*On va merge les données de l'utilisateur existant avec les nouvelles
+      données dans un nouvel objet avec l'operateur spread de js, s'il y a des
+      nouvelles données identiques à l'objet de base elles vont les écraser*/
+      const newUser = {...existingUser, ...data};
+      new User(newUser);
+      return await this.repo.updateUser(id, data);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
