86483a90003065b87cf30c9ce9ac336c77063670
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-02-19 14:12:23 +0100

    add: index and partners controllers files added, admin controller deleted

--- backend/src/controllers/partnersCtrl.js
+++ backend/src/controllers/partnersCtrl.js
@@ -1,17 +1,74 @@
-// Fonctions pour les routes Partenaires
+// Import des services que l'on va appeler dans les controllers
+import PartnerServices from "../services/partnerServices";
 
-const getAllPartners = (req, res) => {
-  res.json({message: 'Page Partenaires'});
-};
+// crée un nouveau partenaire
+// On renvoie le résultat du service, sinon l'erreur est prise en charge par le errorHandler automatiquement
+const createPartner = async (req, res, next) => {
+  try {
+    const data = req.body;
+    const newPartner = await PartnerServices.createPartner({
+      name: data.name,
+      description: data.description,
+      website: data.website,
+      financialAid: data.financialAid,
+      logo: data.logo
+    });
+    res.status(201).json(newPartner);
+  } catch (error) {
+    next(error);
+  }
+}
 
-const getPartnerByID = (req, res) => {
-  // Enregistre l'id dans la variable id
-  const id = req.params.id;
-  res.json({partenaireID: id});
-};
+// Trouve un partenaire avec son ID
+const getPartnerByID = async (req, res, next) => {
+  try {
+    // Enregistre l'id dans la variable id
+    const id = req.params.id;
+    const partner = await PartnerServices.getPartnerByID(id);
+    res.status(200).json(partner);
+  } catch (error) {
+    next(error);
+  }
+}
+
+// Renvoie tous les partenaires
+const getAllPartners = async (req, res, next) => {
+  try {
+    const partners = await PartnerServices.getAllPartners();
+    res.status(200).json(partners);
+  } catch (error) {
+    next(error);
+  }
+}
+
+// Met un jour un partenaire
+const updatePartner = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const data = req.body;
+    const partner = await PartnerServices.updatePartner(id, data);
+    res.status(200).json(partner);
+  } catch (error) {
+    next(error);
+  }
+}
+
+// Supprime un partenaire
+const deletePartner = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const partner = await PartnerServices.deletePartner(id);
+    res.status(200).json(partner);
+  } catch (error) {
+    next(error);
+  }
+}
 
 // Export des fonctions
 export default {
+  createPartner,
+  getPartnerByID,
   getAllPartners,
-  getPartnerByID
+  updatePartner,
+  deletePartner
 };
