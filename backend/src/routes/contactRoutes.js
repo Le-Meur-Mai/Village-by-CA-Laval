4f846aa0ad6f3a3001e92b8f4947681e55738982
Le-Meur-Mai <mai.lm@outlook.fr>
2026-02-26 13:19:10 +0100

    Add: Adding Contact route

--- backend/src/routes/contactRoutes.js
+++ backend/src/routes/contactRoutes.js
@@ -1,19 +1,17 @@
-/*// Importe le package express
+// Importe le package express
 import express from 'express'
 // Importe les controlleurs associés à cette route
 import contactCtrl from '../controllers/contactCtrl.js'
-*/
 
 /*
 Créer un router avec le module express.Router, permet de définir les routes
 dans des fichiers séparés
 */
-/*
+
 const contactRouteur = express.Router();
 
 // Définitions des différentes routes et méthodes
-contactRouteur.post('/', contactCtrl.postContact);
+contactRouteur.post('/', contactCtrl.sendMail);
 
 // Export du routeur
 export default contactRouteur;
-*/
