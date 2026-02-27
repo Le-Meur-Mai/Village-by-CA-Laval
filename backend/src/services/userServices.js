bad54b504c73f00e43298f4f4ba849b25a1a80db
Le-Meur-Mai <mai.lm@outlook.fr>
2026-02-27 16:39:35 +0100

    Update: Adding security with admin verification and hash function

--- backend/src/services/userServices.js
+++ backend/src/services/userServices.js
@@ -9,6 +9,8 @@ import QuoteServices from "./quoteServices.js";
 import prisma from "../prismaClient.js";
 // Import de la classe erreur renvoyant des erreurs personnalisées
 import * as Errors from "../errors/errorsClasses.js";
+// Import de la fonction de hashage du mot de passe
+import hashPassword from "../utils/hashPassword.js";
 
 export default class UserServices {
   constructor() {
@@ -23,7 +25,7 @@ export default class UserServices {
     try {
       // Crée une nouvelle instance pour vérifier la conformité des données
       new User(data);
-      // Importer fonction de hachage et remplacer le mdp de data
+      data.password = await hashPassword(data.password); 
       data.isAdmin = false;
       return await this.userRepo.createUser(data);
     } catch (error) {
@@ -54,19 +56,27 @@ export default class UserServices {
   }
 
   // PATCH mise à jour d'un user
-  async updateUser(id, data) {
+  async updateUser(id, data, isAdmin) {
     try {
       // Vérifie que le User existe
       const existingUser = await this.userRepo.getUserById(id);
       if (!existingUser) {
         throw new Errors.NotFoundError('User not found');
       }
+
+      // Avant de modifier le mot de passe, vérifier que c'est bien un admin.
+      if (data.password && isAdmin) {
+        data.password = await hashPassword(data.password);
+      } else if (data.password && !isAdmin) {
+        throw new Errors.ForbiddenError('Only an admin can modify the password');
+      }
       /*On va merge les données de l'utilisateur existant avec les nouvelles
       données dans un nouvel objet avec l'operateur spread de js, s'il y a des
       nouvelles données identiques à l'objet de base elles vont les écraser*/
       const newUser = {...existingUser, ...data};
       new User(newUser);
-      return await this.userRepo.updateUser(id, data);
+      const updatedUser = await this.userRepo.updateUser(id, data);
+      return {name: updatedUser.name, email: updatedUser.email};
     } catch (error) {
       throw error;
     }
