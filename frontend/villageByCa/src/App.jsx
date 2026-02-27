d49b2425a1993a06f7e0424e4d0b278eeec4c7e4
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-02-27 11:28:58 +0100

    fix: footer now stick to the bottom of the page

--- frontend/villageByCa/src/App.jsx
+++ frontend/villageByCa/src/App.jsx
@@ -1,10 +1,14 @@
+import './App.css'
+
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
+import HeroSection from './components/HeroSection.jsx';
 
 const App = () => {
   return (
-    <div>
+    <div className='app'>
       <Header />
+      <HeroSection />
       <Footer />
     </div>
   )
