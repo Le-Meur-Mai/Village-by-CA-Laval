244c347adfcab8d06aa0008dae55467de9eb0532
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-03-01 11:16:39 +0100

    add: connection page added

--- frontend/villageByCa/src/App.jsx
+++ frontend/villageByCa/src/App.jsx
@@ -7,6 +7,7 @@ import Partenaires from './pages/Partenaires.jsx';
 import Agenda from './pages/Agenda.jsx';
 import Contact from './pages/Contact.jsx';
 import Locaux from './pages/Locaux.jsx';
+import Connexion from './pages/Connexion.jsx';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path='/agenda' element={<Agenda />} />
         <Route path='/locations' element={<Locaux />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Connexion />} />
       </Routes>
   )
 }
---
+++ frontend/villageByCa/src/pages/Connexion.jsx
@@ -0,0 +1,15 @@
+import '../styles/page.css'
+
+import Header from '../components/Header.jsx';
+import Footer from '../components/Footer.jsx';
+
+const Connexion = () => {
+    return (
+        <div className='page'>
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
+export default Connexion;
