0613e6b44329f58675e21286b883551fbf843f58
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-03-01 12:04:03 +0100

    update: hero section updated with current position of title and button

--- frontend/villageByCa/src/components/HeroSection.jsx
+++ frontend/villageByCa/src/components/HeroSection.jsx
@@ -1,11 +1,18 @@
 import '../styles/HeroSection.css';
 
+import Button from './buttons/button';
 import VillageImage from '../assets/HeroImage.png';
 
 const HeroSection = () => {
     return (
         <div className='hero-section-container'>
             <img src= { VillageImage } alt="Vue du Village by CA" className='village-image'/>
+            <div className='top'>
+                <h3 className='title'>Découvrez les Startups de demain</h3>
+            </div>
+            <div className='bottom'>
+                <Button text="Découvrez les Startups" />
+            </div>
         </div>
     )
 }
