6cd7601b4ff107354f13ef5b5b655c671de76269
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-03-02 11:21:44 +0100

    add: first API call added, the current URL is for test purpose

--- frontend/villageByCa/src/pages/Startups.jsx
+++ frontend/villageByCa/src/pages/Startups.jsx
@@ -2,11 +2,32 @@ import '../styles/page.css'
 
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
+import { useEffect, useState } from 'react';
 
 const Startups = () => {
+    /* On créer un useState qui va contenir toutes nos Startups */
+    const [startups, setStartups] = useState([]);
+
+    /* On va chercher toutes nos startups dans la base de données lors
+    du premier chargement de la page */
+    useEffect (() => {
+        const fetchStartups = async () => {
+            const result = await fetch('http://localhost:3000/admin/users');
+            /* On convertit la promesse en json*/
+            const jsonResult = await result.json()
+            setStartups(jsonResult);
+            }
+            /* On appelle la fonction */
+            fetchStartups();
+        }, []);
+
     return (
         <div className='page'>
             <Header />
+            {/* On parcourt l'objet avec map, on assigne un id pour identifier chaque startups*/}
+            {startups.map(startup => (
+                <p key={ startup.id }>{ startup.name }</p>
+            ))}
             <Footer />
         </div>
     )
