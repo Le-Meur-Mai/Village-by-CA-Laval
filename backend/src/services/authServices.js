db3921ed1046913fbcaee2d285fcee432c0f06ba
Le-Meur-Mai <mai.lm@outlook.fr>
2026-03-02 11:23:35 +0100

    Update: Removing unecessary function

--- backend/src/services/authServices.js
+++ backend/src/services/authServices.js
@@ -4,10 +4,6 @@ import UserRepository from "../repositories/UserRepository.js";
 import prisma from "../prismaClient.js";
 // Import de la classe erreur renvoyant des erreurs personnalisées
 import * as Errors from "../errors/errorsClasses.js"
-// Import de la fonction cloudinary pour envoyer les images sur l'hébergeur
-import uploadPictureToCloudinary from "../utils/uploadToCloudinary.js";
-// Importation de la config Cloudinary pour pouvoir supprimer des images
-import cloudinary from "../../config/cloudinary.js";
 // Importation de la fonction pour checker le mot de passe
 import checkPassword from "../utils/checkPassword.js";
 
@@ -28,42 +24,6 @@ export default class AuthServices {
           return {id: existingUser.id, isAdmin: existingUser.isAdmin};
         }
       })
-    } catch (error) {
-      throw error;
-    }
-  }
-  
-  // Get the user profile
-  async getProfile(id) {
-    try {
-      const existingUser = await this.userRepo.getUserById(id);
-      if (!existingUser) {
-        throw new Errors.NotFoundError('User not found');
-      } else {
-        const profile = {
-          user: {name: existingUser.name, email: existingUser.email}
-        };
-        if (existingUser.startUp !== null) {
-          profile.startUp = {
-            id: existingUser.startUp.id,
-            name: existingUser.startUp.name,
-            description: existingUser.startUp.description,
-            isAlumni: existingUser.startUp.isAlumni,
-            website: existingUser.startUp.website,
-            logo: existingUser.startUp.logo.secureUrl,
-            descriptionPicture: existingUser.startUp.descriptionPicture.secureUrl,
-            types: existingUser.startUp.types
-          };
-        } else {
-          profile.startUp = null;
-        }
-        if (existingUser.quotes.length > 0) {
-          profile.quotes = existingUser.quotes;
-        } else {
-          profile.quotes = [];
-        }
-        return profile;
-      }
     } catch (error) {
       throw error;
     }
