a7e8658bf1024b23a4e7597db3aaef800536295f
Le-Meur-Mai <mai.lm@outlook.fr>
2026-03-04 19:41:09 +0100

    Update: Updating the cards with the adequate data format

--- frontend/villageByCa/src/components/Card.jsx
+++ frontend/villageByCa/src/components/Card.jsx
@@ -1,13 +1,17 @@
 import '../styles/Card.css'
 
-const Card = ({title = "Titre", description = "Description"}) => {
+const Card = ({name = "Startup", description = "Description", logo, color = "CCF2B1", onClick}) => {
     return (
-        <div className="card">
-            <div className='color-card'></div>
+        <div className="card" onClick={onClick}>
+            <div className='color-card' style={{ backgroundColor: `#${color}` }}></div>
             <div className='card-content'>
-                <div className='card_logo'></div>
-                <h4 className="card-title">{ title }</h4>
-                <p className="card-description">{ description }</p>
+                <div className='card_logo'>
+                    {logo && <img src={logo} alt={name} />}
+                </div>
+                <h4 className="card-title">{ name }</h4>
+                <p className="card-description">{ description.length > 100
+                ? description.slice(0, 200) + "..."
+                : description}</p>
             </div>
         </div>
     )
