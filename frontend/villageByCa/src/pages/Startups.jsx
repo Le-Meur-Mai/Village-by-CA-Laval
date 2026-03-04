86e7ac4ff079d626c8144d489c8bc3ef5b837787
Le-Meur-Mai <mai.lm@outlook.fr>
2026-03-04 19:44:24 +0100

    Update: The startups are shown into cards, add filters and popup cards to the page

--- frontend/villageByCa/src/pages/Startups.jsx
+++ frontend/villageByCa/src/pages/Startups.jsx
@@ -2,28 +2,56 @@ import '../styles/page.css'
 
 import Header from '../components/Header.jsx';
 import PresentationPage from '../components/PresentationPage.jsx';
+import StartupPopup from '../components/StartupPopUp.jsx';
 import PreFooter from '../components/PreFooter.jsx';
 import Footer from '../components/Footer.jsx';
 import { useEffect, useState } from 'react';
 import Card from '../components/Card.jsx';
 import Tag from '../components/Tag.jsx';
 
+
 const Startups = () => {
-    /* On créer un useState qui va contenir toutes nos Startups */
+    /* On créer un useState qui va contenir toutes nos Startups et nos types*/
     const [startups, setStartups] = useState([]);
+    const [types, setTypes] = useState([])
+    const [showPopup, setShowPopup] = useState(false);
+    const [popupData, setPopupData] = useState(null);
+    // Pour le filtrage
+    const [selectedType, setSelectedType] = useState(null);
+    const [showAlumni, setShowAlumni] = useState(false);
 
     /* On va chercher toutes nos startups dans la base de données lors
     du premier chargement de la page */
     useEffect (() => {
-        const fetchStartups = async () => {
-            const result = await fetch('http://localhost:3000/admin/users');
+        const fetchStartupsAndTypes = async () => {
+            const result = await fetch('http://localhost:3000/startups');
             /* On convertit la promesse en json*/
             const jsonResult = await result.json()
-            setStartups(jsonResult);
+            setStartups(jsonResult.startUps);
+            setTypes(jsonResult.types); 
             }
             /* On appelle la fonction */
-            fetchStartups();
+            fetchStartupsAndTypes();
         }, []);
+
+    // Fonction pour ouvrir le popup
+    const openPopup = async (id) => {
+        const res = await fetch(`http://localhost:3000/startups/${id}`);
+        const data = await res.json();
+        setPopupData(data);
+        setShowPopup(true);
+    };
+
+    // Fonction pour filtrer les startups
+    const filteredStartups = startups.filter(startup => {
+        if (selectedType && !startup.types.some(t => t.name === selectedType.name)) {
+            return false;
+        }
+        if (showAlumni && !startup.isAlumni) {
+            return false;
+        }
+        return true;
+    });
 
     return (
         <div className='page'>
@@ -35,12 +63,44 @@ const Startups = () => {
                     et leurs ambitions. Entre mentorat, mise en réseau et soutien stratégique, nous créons 
                     les conditions idéales pour accélérer leurs projets. Cette dynamique collective fait émerger 
                     des innovations qui transforment durablement nos territoires.' />
+                <div className="filters">
+                    <Tag 
+                        name="Alumni"
+                        color="88d783"
+                        active={showAlumni}
+                        onClick={() => setShowAlumni(!showAlumni)}
+                    />
+                    {types.map(type => (
+                        <Tag 
+                            key={type.id}
+                            name={type.name}
+                            color={type.color}
+                            active={selectedType?.name === type.name}
+                            // Interrupteur, désactive un filtre ou le réactive
+                            onClick={() => setSelectedType(
+                                selectedType?.name === type.name ? null : type
+                            )}
+                        />
+                    ))}
+                </div>
                 {/* On parcourt l'objet avec map, on assigne un id pour identifier chaque startups*/}
-                {startups.map(startup => (
-                    <p key={ startup.id }>{ startup.name }</p>
+                {filteredStartups.map(startup => (
+                    <Card 
+                        key={startup.id}
+                        name={startup.name}
+                        description={startup.description}
+                        logo={startup.logo}
+                        color={startup.types[0]?.color}
+                        onClick={() => openPopup(startup.id)}
+                    />
                 ))}
-                <Tag />
-                <Card />
+                {/*Pour activer le pop-up*/}
+                {showPopup && (
+                    <StartupPopup 
+                        data={popupData}
+                        onClose={() => setShowPopup(false)}
+                    />
+                )}
             </main>
             <PreFooter 
                 title="Envie de vous faire accompagner ?" 
