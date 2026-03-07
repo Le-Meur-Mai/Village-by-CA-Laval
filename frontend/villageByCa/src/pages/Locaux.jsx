edd86910a5be0c6f3aa9ad4435c5b56b78478719
Le-Meur-Mai <mai.lm@outlook.fr>
2026-03-08 00:24:50 +0100

    Update: Functionnal locations route in the front

--- frontend/villageByCa/src/pages/Locaux.jsx
+++ frontend/villageByCa/src/pages/Locaux.jsx
@@ -1,11 +1,31 @@
-import '../styles/page.css'
+import '../styles/page.css';
+import '../styles/LocationCard.css';
+import video from "../assets/videoVillageByCa.png";
 
+import LocationCard from '../components/LocationCard.jsx';
 import Header from '../components/Header.jsx';
 import PresentationPage from '../components/PresentationPage.jsx';
 import PreFooter from '../components/PreFooter.jsx';
 import Footer from '../components/Footer.jsx';
 
+import { useEffect, useState } from 'react';
+
 const Locaux = () => {
+    /* On créer un useState qui va contenir toutes nos partenaires*/
+    const [locations, setLocations] = useState([]);
+
+    // On va récupérer les partenaires après le chargement de la page
+    useEffect (() => {
+            const fetchLocations = async () => {
+                const result = await fetch('http://localhost:3000/locations');
+                /* On convertit la promesse en json*/
+                const jsonResult = await result.json()
+                setLocations(jsonResult);
+                }
+                /* On appelle la fonction */
+                fetchLocations();
+    }, []);
+        
     return (
         <div className='page'>
             <Header />
@@ -15,6 +35,21 @@ const Locaux = () => {
                     text="Nos startups s’épanouissent dans nos locaux, où elles peuvent pleinement 
                     se développer et créer un réseau. C’est un cadre idéal pour rassembler et dévelloper 
                     votre startup." />
+                <h2 className='section-title-left'>Les Locaux:</h2>
+                <img className='paragraph-center' src={video} alt="Découverte des bureaux du Village by CA"/>
+                <h2 className='section-title-right'>Location de salles/bureaux:</h2>
+                <div className="locations-container">
+                    {locations.map(location => (
+                                <LocationCard 
+                                key={location.id}
+                                title={location.title}
+                                price={location.price}
+                                size={location.size}
+                                picture={location.picture}
+                                id={location.id}
+                                />
+                    ))}
+                </div>
             </main>
             <PreFooter 
                 title="Vous voulez louer une de nos salles ?" 
