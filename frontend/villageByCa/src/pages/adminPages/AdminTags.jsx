0edd237bbe4506c612ae768102406133887dfb2c
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-03-12 23:42:53 +0100

    update: types are now displayed to the admin types page

--- frontend/villageByCa/src/pages/adminPages/AdminTags.jsx
+++ frontend/villageByCa/src/pages/adminPages/AdminTags.jsx
@@ -2,12 +2,42 @@ import '../../styles/page.css'
 
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import PresentationPage from '../../components/PresentationPage';
+import { useEffect, useState } from 'react';
+import Tag from '../../components/Tag';
 
 const AdminTags = () => {
+
+    const [allTypes, setAllTypes] = useState([]);
+
+    useEffect(() => {
+        const getAllTypes = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/admin/types',  {
+                    method: 'GET',
+                    credentials: 'include'
+                })
+    
+                const data = await response.json();
+    
+                setAllTypes(data)
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        getAllTypes()
+    }, [])
+
     return (
         <div className="page">
             <Header />
             <main>
+                <PresentationPage title='Gestion des Types' text='' />
+                <div>
+                    {allTypes.map(type =>
+                        <Tag key={ type.id } name={ type.name } color={ type.color } />
+                    )}
+                </div>
     
             </main>
             <Footer />
