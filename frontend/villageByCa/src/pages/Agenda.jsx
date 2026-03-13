1c75589c2135aa2520f486cb860d3fa3ad12e4db
Le-Meur-Mai <mai.lm@outlook.fr>
2026-03-13 23:52:45 +0100

    Update: add new fields for the PostCard

--- frontend/villageByCa/src/pages/Agenda.jsx
+++ frontend/villageByCa/src/pages/Agenda.jsx
@@ -35,7 +35,9 @@ const Agenda = () => {
                 <h2 className='section-title-right'>Actualités du Village</h2>
                 <div className='quote-align'>
                     {posts.map(post => (
-                        <PostCard post={post}/>
+                        <PostCard post={post}
+                        text={'Lire'}
+                        path={`/agenda/${post.id}`}/>
                     ))}
                 </div>
             </main>
