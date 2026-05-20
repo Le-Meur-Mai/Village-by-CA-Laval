6c410c710538dc3d60d00cb879e7ad9bf9e557a3
Le-Meur-Mai <mai.lm@outlook.fr>
2026-05-20 16:55:13 +0200

    Update: Updating to a better asset and optimized the images that are displayed for better performances

--- frontend/villageByCa/src/components/PostCard.jsx
+++ frontend/villageByCa/src/components/PostCard.jsx
@@ -1,8 +1,11 @@
 import Button from "./buttons/button.jsx";
 import "../styles/PostCard.css";
-import villageByCa from '../assets/logo_village_by_ca.png';
 
 export default function PostCard({ post, text, path, onClick }) {
+
+    // Logo par défaut si aucune image de post n'est fourni, host sur Cloudinary pour pouvoir l'optimiser
+    const villageByCa = "https://res.cloudinary.com/dwc7gkjyk/image/upload/w_365,dpr_auto,f_auto,q_auto/v1779287482/logo_village_by_ca_e3pngc.webp"
+
     // On prend la date du createdAt et on le met en format lisible car de base il est en ISO
     const date = new Date(post.createdAt).toLocaleDateString("fr-FR", {
         day: "2-digit",
@@ -14,7 +17,7 @@ export default function PostCard({ post, text, path, onClick }) {
         <div className="post-card">
             {post.picture &&(
                 <div className="post-image">
-                    <img src={post.picture} alt={post.title} />
+                    <img src={post.picture.replace('/upload/', '/upload/w_365,dpr_auto,f_auto,q_auto/')} alt={post.title} />
                 </div>
             )}
             {!post.picture &&(
