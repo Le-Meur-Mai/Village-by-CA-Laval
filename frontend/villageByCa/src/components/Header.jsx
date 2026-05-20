f3de6381ae10acecbe75e500ac300a1ee953693c
Le-Meur-Mai <mai.lm@outlook.fr>
2026-05-20 16:51:09 +0200

    Update: adding a more optimized picture to the header

--- frontend/villageByCa/src/components/Header.jsx
+++ frontend/villageByCa/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { useAuth } from "../contexts/AuthContext";
 import { useState } from "react";
 
 import '../styles/Header.css'
-import logoBlanc from '../assets/logo_blanc.png';
+import logoBlanc from '../assets/logo_blanc.webp';
 
 const Header = () => {
     const { auth, loading } = useAuth();
